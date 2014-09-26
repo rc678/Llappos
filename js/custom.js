bcdb242c3eecbf669427609b852d84062d88f242
rchaudhry <rosheenmail@gmail.com>
2014-09-26 01:43:23 -0400

    added some jQuery UI for dialog box.

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     //test to see if jQuery linked
+    createDialog();
+    hoverEffect();
+});
+
+/*enlarges the images when the mouse hovers over it*/
+function hoverEffect() {
     $('#resizeLeft').hover(enlarge, originalSize);
     $('#resizeMiddle').hover(enlarge, originalSize);
     $('#resizeRight').hover(enlarge, originalSize);
-});
+}
 
 /*enlarges any element by 20%*/
 function enlarge(){
@@ -12,5 +18,18 @@ function enlarge(){
 
 /*returns an element to it's original size*/
 function originalSize(){
-     $(this).css({height: "", width: ""});
+    $(this).css({height: "", width: ""});
+}
+
+/*creates a dialog box when picture is clicked*/
+function createDialog(){
+    $( "#resizeLeft" ).click(function() {
+    $( '#dialog' ).dialog();
+    });
+    $( "#resizeMiddle" ).click(function() {
+    $( '#dialog' ).dialog();
+    });
+    $( "#resizeRight" ).click(function() {
+    $( '#dialog' ).dialog();
+    });
 }
