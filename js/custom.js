6fe238c9596f88bcf5d5275a7d3ea05f76f4e389
rchaudhry <rosheenmail@gmail.com>
2014-09-25 20:39:28 -0400

    fixed navbar and footer

--- js/custom.js
+++ js/custom.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
     $('#resizeRight').hover(enlarge, originalSize);
 });
 
+/*enlarges any element by 20%*/
 function enlarge(){
     $(this).css({height: '+=20%', width: '+=20%'});
 }
 
+/*returns an element to it's original size*/
 function originalSize(){
      $(this).css({height: "", width: ""});
 }
